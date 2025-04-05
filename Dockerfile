34dc51c10301232af2100b78067f0ffeefd120b0
Bartek Igielski <igloczek@gmail.com>
2025-04-05 11:46:48 +0200

    sample astro app

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,20 @@
-FROM oven/bun:1.2.8
+FROM oven/bun:1.2.8 AS base
 
-WORKDIR /app
+FROM base AS builder
+
+WORKDIR /usr/src/app
+
+COPY package.json bun.lock ./
+RUN bun install
 
 COPY . .
+RUN bun run build
 
-ENTRYPOINT ["bun", "index.ts"]
+FROM base
+WORKDIR /usr/src/app
+COPY --from=builder /usr/src/app .
+
+ENV NODE_ENV=production
+ENV HOST=0.0.0.0
+ENV PORT=3000
+ENTRYPOINT [ "bun", "./dist/server/entry.mjs" ]
---
+++ src/pages/form-data.ts
@@ -0,0 +1,17 @@
+import type { APIRoute } from "astro";
+
+export const POST: APIRoute = async function ({ request }) {
+  try {
+    const formData = await request.formData();
+    console.log(formData);
+
+    return new Response(JSON.stringify(Object.fromEntries(formData)), {
+      status: 200,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  } catch (error) {
+    return new Response("Error", { status: 500 });
+  }
+};
